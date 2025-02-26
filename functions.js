f5496c17c3616046b88540d8d3c6190ce8f8115b
Nchege Samuel <74881592+samuelnchege@users.noreply.github.com>
2025-02-26 09:27:37 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -53,16 +53,3 @@ function StartWriter(i) {
 setTimeout(function () {
   StartWriter(0);
 }, 100);
-
-// The Contact me session on submit with EmailJS service
-function sendEmail() {
-  let emailDetails = {
-    name: document.getElementById("fname").value,
-    email: document.getElementById("email").value,
-    subject: document.getElementById("subject").value,
-    message: document.getElementById("message").value,
-  };
-  emailjs
-    .send("service_3yvwauj", "template_0zgyr9o", emailDetails)
-    .then(alert("Message sent Successfully"));
-}
