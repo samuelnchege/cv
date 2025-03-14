e1a90a9c50e3369f5e505293d8e0f8d9ae4f1454
samuelnchege <nchegesamuel@gmail.com>
2025-03-14 17:08:36 +0100

    Deleted some irrelevant files and added some- also added the download cv file and

--- functions.js
+++ functions.js
@@ -53,3 +53,13 @@ function StartWriter(i) {
 setTimeout(function () {
   StartWriter(0);
 }, 100);
+
+// Downloading my cv pdf format document
+function downloadPdf() {
+  var link = document.createElement('a');
+  link.href = 'Samuel Anyaja Nchege_cv.pdf';
+  link.download = 'Samuel Anyaja Nchege_cv.pdf';
+  link.click();
+  alert(`CV - Download successfull!
+Please view in downloads(Samuel Anyaja Nchege_cv.pdf)`)
+}
